07ef0e85a2afacef9dda7f78ac460527c4407cfb
Adán SDPC <adan.sdpc@gmail.com>
2019-09-04 14:59:03 +0200

    chore(truffle-config): clean-up testnets configuration

--- truffle-config.js
+++ truffle-config.js
@@ -18,12 +18,6 @@
  *
  */
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
-
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
@@ -36,68 +30,43 @@ module.exports = {
    */
 
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
     development: {
       provider: require("ganache-cli").provider({ gasLimit: 100000000 }),
       network_id: "*",
     },
-
-    // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websockets: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
-
-    // Useful for private networks
-    // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    // network_id: 2111,   // This network is yours, in the cloud.
-    // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
-  },
-
-  // Set default mocha options here, use special reporters etc.
-  mocha: {
-  // timeout: 100000
-  },
-
-  // Configure your compilers
-  compilers: {
-    solc: {
-    // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
-    // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-    // settings: {          // See the solidity docs for advice about optimization and evmVersion
-    //  optimizer: {
-    //    enabled: false,
-    //    runs: 200
-    //  },
-    //  evmVersion: "byzantium"
-    // }
+    rinkeby: {
+      network_id: 4,
+      host: "localhost",
+      port: 9004,
+      gas: 6500000,
     },
+    ropsten: {
+      network_id: 3,
+      host: "localhost",
+      port: 9005,
+      gas: 6500000,
+    },
+    goerli: {
+      network_id: 5,
+      host: "localhost",
+      port: 9006,
+      gas: 6500000,
+    },
+  },
+
+  // The `solc` compiler is set to optimize output bytecode with 200 runs, which is the standard these days
+  compilers: {
+    solc: { optimizer: { enabled: true, runs: 200 } },
+  },
+
+  // This plugin allows to verify the source code of your contracts on Etherscan with this command:
+  // ETHERSCAN_API_KEY=<your_etherscan_api_key> truffle run verify <contract_name> --network <network_name>
+  plugins: [
+    "truffle-plugin-verify",
+  ],
+
+  // This is just for the `truffle-plugin-verify` to catch the API key
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_API_KEY,
   },
 }
