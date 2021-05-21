5194bcd02970f5fcb255a9bef94dce1695dd9926
Mario Cao <mariocao@gmail.com>
2021-05-21 10:43:02 +0200

    chore: update solc version to 0.8.4

--- truffle-config.js
+++ truffle-config.js
@@ -59,7 +59,7 @@ module.exports = {
   // The `solc` compiler is set to optimize output bytecode with 200 runs, which is the standard these days
   compilers: {
     solc: {
-      version: "0.6.12",
+      version: "0.8.4",
       settings: { optimizer: { enabled: true, runs: 200 } } },
   },
 
